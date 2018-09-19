685b6be003c5aaf771d8b9b9e0a18d97564b7c82
Crossley Rozario <crozariodev@gmail.com>
2018-09-19 14:59:16 -0400

    init

---
+++ frontend/main.js
@@ -0,0 +1,10 @@
+
+function loginButtonOnClick() {
+    var username =  document.getElementsByName('username')[0].value;
+    var password = document.getElementsByName('password')[0].value;
+
+
+    console.log(username);
+    console.log(password);
+
+}

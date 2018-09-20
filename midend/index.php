ab5bf34b7f4d5438a6cdaf61155fe70e6ea0b8c9
Al Furqan Khan <furqankhan123.alfurqankhan@gmail.com>
2018-09-19 22:03:50 -0400

    added JSON response for njit auth

--- midend/index.php
+++ midend/index.php
@@ -16,39 +16,41 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
 $response = curl_exec($ch);
 if (strpos($response, 'Please select a MyHousing System to sign into:') == true){
-	echo "Successful Login";
+	header('Content-type: application/json');
+  echo json_encode("Successful Login")
 } else {
-	echo "Failed Login";
+  header('Content-type: application/json');
+	echo json_encode("Failed Login");
 }
 curl_close($ch);
 }
 
-function auth($user, $pass){
-$data = array(
-    'ucid' => $user,
-    'pass' => $pass
-);
-#TODO: add backend php page URL
-$url = "";
-$ch = curl_init($url);
-
-$postString = http_build_query($data, '', '&');
-
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$response = curl_exec($ch);
-#TODO: add if-else logic for login response from backend
-
-curl_close($ch);
-}
+// function auth($user, $pass){
+// $data = array(
+//     'ucid' => $user,
+//     'pass' => $pass
+// );
+// #TODO: add backend php page URL
+// $url = "";
+// $ch = curl_init($url);
+//
+// $postString = http_build_query($data, '', '&');
+//
+// curl_setopt($ch, CURLOPT_POST, 1);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//
+// $response = curl_exec($ch);
+// #TODO: add if-else logic for login response from backend
+//
+// curl_close($ch);
+// }
 
 $USER = $_POST['username'];
 $PASS = $_POST['password'];
 
 auth_njit($USER, $PASS);
-auth($USER, $PASS);
+//auth($USER, $PASS);
 
 #TODO: send JSON data to the frontend php URL
 ?>

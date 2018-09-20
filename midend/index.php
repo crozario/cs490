e39fbc51878b723534bc339a07f2bbd6dbc43c7b
Al Furqan Khan <furqankhan123.alfurqankhan@gmail.com>
2018-09-19 22:28:33 -0400

    added json_encode()

--- midend/index.php
+++ midend/index.php
@@ -25,32 +25,14 @@ if (strpos($response, 'Please select a MyHousing System to sign into:') == true)
 curl_close($ch);
 }
 
-// function auth($user, $pass){
-// $data = array(
-//     'ucid' => $user,
-//     'pass' => $pass
-// );
 // #TODO: add backend php page URL
-// $url = "";
-// $ch = curl_init($url);
-//
-// $postString = http_build_query($data, '', '&');
-//
-// curl_setopt($ch, CURLOPT_POST, 1);
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//
-// $response = curl_exec($ch);
-// #TODO: add if-else logic for login response from backend
-//
-// curl_close($ch);
-// }
 
-$USER = $_POST['username'];
-$PASS = $_POST['password'];
+// #TODO: add if-else logic for login response from backend
+
+$USER = 'user';//$_POST['username'];
+$PASS = 'pass';//$_POST['password'];
 
 auth_njit($USER, $PASS);
-//auth($USER, $PASS);
 
 #TODO: send JSON data to the frontend php URL
 ?>

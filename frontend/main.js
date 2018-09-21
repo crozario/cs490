f31ce783320ef89673be04e236bd6c63af364c99
Crossley Rozario <crozariodev@gmail.com>
2018-09-20 20:33:10 -0400

    added 2 header lines

--- frontend/main.js
+++ frontend/main.js
@@ -5,6 +5,7 @@ function loginButtonOnClick() {
     var username = document.getElementsByName('username')[0].value;
     var password = document.getElementsByName('password')[0].value;
     var vars = "username="+username+"&password="+password;
+    var status_id = document.getElementById('status');
 
     // fetch('/main.php', {
     //         method: 'POST',
@@ -38,25 +39,32 @@ function loginButtonOnClick() {
     console.log(vars);
 
     var req = new XMLHttpRequest();
-    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    req.onreadystatechange = sent_request_to_php;
+    
     req.open("POST", "main.php", true);
+    // req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    req.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    req.getAllResponseHeaders();
     req.send(vars); 
+
+    req.onreadystatechange = function() {
+        if(req.readyState == 4) {
+        
+            if (req.status == 200) {
+                console.log("data received");
+                // alert(req.responseText);
+                status_id.innerHTML = req.responseText;
+                // status_id.innerHTML = 'Received';
+            } else {
+                console.log("data failed");
+                status_id.innerHTML = 'An error occurred during your request: ' +  req.status + ' ' + req.statusText;
+            }
+            
+        }  
+    }
+
+    
 }
 
-function sent_request_to_php() {
-    if(req.readyState == 4) {
-        var status_id = document.getElementById('status');
-        if (req.status == 200) {
-            console.log("data received");
-            // alert(req.responseText);
-            status_id.innerHTML = req.responseText;
-            // status_id.innerHTML = 'Received';
-        } else {
-            console.log("data failed");
-            status_id.innerHTML = 'An error occurred during your request: ' +  req.status + ' ' + req.statusText;
-        }
-        
-    }  
-}
+// function sent_request_to_php() {
+    
+// }

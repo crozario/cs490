75b5cf74202a6fdfd88602fab62d2580c11628c7
Al Furqan Khan <furqankhan123.alfurqankhan@gmail.com>
2018-09-22 00:03:50 -0400

    testing DB response

--- midend/index.php
+++ midend/index.php
@@ -5,8 +5,9 @@ $data = array(
     'ucid' => $user,
     'pass' => $pass
 );
+#https://aevitepr2.njit.edu/myhousing/login.cfm
 
-$url = "https://aevitepr2.njit.edu/myhousing/login.cfm";
+$url = "https://web.njit.edu/~eo65/CS490_project/cs490_alpha/backend/login.php";
 $ch = curl_init($url);
 
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -15,20 +16,21 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
 $response = curl_exec($ch);
-if (strpos($response, 'Please select a MyHousing System to sign into:') == true){
-  echo json_encode("Successful Login");
-} else {
-	echo json_encode("Failed Login");
-}
-curl_close($ch);
+print_r($response);
+// if (strpos($response, 'Please select a MyHousing System to sign into:') == true){
+//   echo json_encode("Successful Login");
+// } else {
+// 	echo json_encode("Failed Login");
+// }
+// curl_close($ch);
 }
 
 #TODO: add backend php page URL
 
 #TODO: add if-else logic for login response from backend
 
-$USER = $_POST['username'];
-$PASS = $_POST['password'];
+$USER = 'ak697';//$_POST['username'];
+$PASS = '123456';//$_POST['password'];
 
 auth_njit($USER, $PASS);
 

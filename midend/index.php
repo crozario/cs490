4a7b60f1c68b746b4c5d156db28de498e498350b
Al Furqan Khan <furqankhan123.alfurqankhan@gmail.com>
2018-09-22 00:15:05 -0400

    testing db response

--- midend/index.php
+++ midend/index.php
@@ -25,6 +25,31 @@ print_r($response);
 // curl_close($ch);
 }
 
+function auth_db($user, $pass){
+$data = array(
+    'username' => $user,
+    'password' => $pass
+);
+#https://aevitepr2.njit.edu/myhousing/login.cfm
+
+$url = "https://web.njit.edu/~eo65/CS490_project/cs490_alpha/backend/login.php";
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+$response = curl_exec($ch);
+print_r($response);
+// if (strpos($response, 'Please select a MyHousing System to sign into:') == true){
+//   echo json_encode("Successful Login");
+// } else {
+// 	echo json_encode("Failed Login");
+// }
+// curl_close($ch);
+}
+
 #TODO: add backend php page URL
 
 #TODO: add if-else logic for login response from backend
@@ -33,6 +58,7 @@ $USER = 'ak697';//$_POST['username'];
 $PASS = '123456';//$_POST['password'];
 
 auth_njit($USER, $PASS);
+auth_db($USER, $PASS);
 
 #TODO: send JSON data to the frontend php URL
 ?>

467901e895277c09f0e391e34078b4f885677810
Elizabeth Oviedo <eo65@afsaccess1.njit.edu>
2018-10-14 17:35:09 -0400

    backend beta

---
+++ beta/backend/db.php
@@ -0,0 +1,11 @@
+<?php
+//Creates a connection to server
+//@param: database name
+	$servername = "*********";
+	$username = "*********";
+	$dbpassword = "*********";
+	$dbname = "*********";
+		
+	//Create connection
+	$db = new mysqli($servername, $username, $dbpassword, $dbname);
+?>
---
+++ beta/backend/login.php
@@ -0,0 +1,27 @@
+<?php
+	
+	include "db.php";
+
+	if ($db->connect_error) {
+ 		die('Connection failed: ' . $db->connect_error);
+	}
+
+	$user = "jk56";//$_POST['username'];
+	$pass = sha1("alpha");//sha1($_POST['password']); 
+
+	if (isset($user) && isset($pass)) {
+		$result = mysqli_query($db, "SELECT * FROM Login WHERE user = '$user' && password = '$pass'");
+		if (mysqli_num_rows($result) == 1) {
+        	$row = $result->fetch_assoc();
+        	printf ('{"role": "%s"}', $row['role']);
+		}
+		else {
+			echo '{"denied":"Wrong username or password"}';
+		}
+	}
+	else {
+		echo '{"Error":"No input"}';
+	}
+
+?>
+

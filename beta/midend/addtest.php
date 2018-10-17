aded07c4aceb53cffdfad6b8ae5f8a1ce5f424ab
furqankhan123 <alfurqankhan@gmail.com>
2018-10-17 01:54:20 -0400

    added loop to read in all questions

--- beta/midend/addtest.php
+++ beta/midend/addtest.php
@@ -6,18 +6,30 @@
  * Time: 12:10 AM
  */
 
+//probably should have testId, questionIds, instructorId
+
+$data = array();
+//foreach question add to data array
+foreach ($_POST as $key => $value) {
+    //do something
+    echo $key . ' has the value of ' . $value . '<br>';
+    $data[$key]=$value;
+}
+var_dump($data);
+
+//POST Exam data to backend, backend creates Exam entry
 function sendTest($data){
 
-    #$url = "https://web.njit.edu/~eo65/CS490_project/cs490_alpha/backend/login.php";
     $url = "backend";
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    var_dump($data);
-    echo '<br>';
+//    var_dump($data);
+//    echo '<br>';
 
-    $response = curl_exec($ch) or die("Curl to backendReceiveQuestion didn't work");
+    $response = curl_exec($ch) or die("Curl to backend_add_test didn't work");
+    //backend should probably print success or fail for adding test
     print_r($response );
 //    if (strpos($response, 'Welcome') == true){
 //        $JSON['db'] = "success";

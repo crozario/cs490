dad911b281b227f6c3474e501683d062926091d1
furqankhan123 <alfurqankhan@gmail.com>
2018-11-20 14:34:33 -0500

    fixed updategrade

--- beta/midend/updategrade.php
+++ beta/midend/updategrade.php
@@ -1,16 +1,53 @@
 <?php
-
 $data = array(
-    $user => $_POST['user'],
-    $exam => $_POST['exam'],
-    $points => $_POST['points'],
-    $comments => $_POST['comments']
+    'user' => $_POST['user'],
+    'exam' => $_POST['exam']
 );
+$info = "";
+$url = "https://web.njit.edu/~eo65/cs490/beta/backend/showstudentanswer.php";
+update($info, $data, $url);
+//echo $info;
+//exit();
+$obj = json_decode($info);
+//array to store all the exam answers
+$answers = array();
 
-showGrades($data);
-function showGrades($data){
+foreach ($obj as $item)
+{
+    $answers[] = $item->answer;
+    //add constraint array later
+//    echo 'question: ' . $question . ' points: '.$points . '<br>';
+}
+//var_dump($answers);
 
-    $url = "https://web.njit.edu/~eo65/cs490/beta/backend/updategrade.php";
+$comments = $_POST['comments'];
+$points = $_POST['points'];
+process($comments);
+process($points);
+
+$url = "https://web.njit.edu/~eo65/cs490/beta/backend/updategrade.php";
+
+for ($x=0; $x<count($comments);$x++){
+    echo "post: ". $x.'<br>';
+    $data = array(
+        'user' => $_POST['user'],
+        'exam' => $_POST['exam'],
+        'points' => $points[$x],
+        'comments' => $comments[$x],
+        'answer'=> $answers[$x]
+    );
+    update($info, $data, $url);
+    var_dump($data);
+}
+
+//var_dump($_POST);
+
+function process(&$str){
+    $str = trim($str,"\"[]");
+    $str = explode("\",\"", $str);
+}
+
+function update(&$info, $data, $url){
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
@@ -18,12 +55,7 @@ function showGrades($data){
 //    var_dump($data);
 //    echo '<br>';
 
-    $response = curl_exec($ch) or die("Curl to backend_get_questions didn't work");
-    print_r($response );
-//    if (strpos($response, 'Welcome') == true){
-//        $JSON['db'] = "success";
-//    } else {
-//        $JSON['db'] = "fail";
-//    }
+    $info = curl_exec($ch) or die("Curl to backend_get_questions didn't work");
+    print_r($info);
     curl_close($ch);
 }
